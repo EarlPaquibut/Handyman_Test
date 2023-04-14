dfaef397a1dec117b1c95cc6cf5ffc67f2d8d992
Earl <earlpaquibut@gmail.com>
2023-04-14 20:22:07 +0800

    DB Test

---
+++ HomeownerSignUpForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Handyman
+{
+    partial class HomeownerSignUpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtMunicipality = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtBarangay = new System.Windows.Forms.TextBox();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.txtContactNumber = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblManucipality = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblBarangay = new System.Windows.Forms.Label();
+            this.Street = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHomeownerSignUp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(192, 25);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(192, 60);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // txtMunicipality
+            // 
+            this.txtMunicipality.Location = new System.Drawing.Point(192, 95);
+            this.txtMunicipality.Name = "txtMunicipality";
+            this.txtMunicipality.Size = new System.Drawing.Size(100, 20);
+            this.txtMunicipality.TabIndex = 3;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(192, 132);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 4;
+            // 
+            // txtBarangay
+            // 
+            this.txtBarangay.Location = new System.Drawing.Point(192, 168);
+            this.txtBarangay.Name = "txtBarangay";
+            this.txtBarangay.Size = new System.Drawing.Size(100, 20);
+            this.txtBarangay.TabIndex = 5;
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(192, 206);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(100, 20);
+            this.txtStreet.TabIndex = 6;
+            // 
+            // txtContactNumber
+            // 
+            this.txtContactNumber.Location = new System.Drawing.Point(192, 243);
+            this.txtContactNumber.Name = "txtContactNumber";
+            this.txtContactNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtContactNumber.TabIndex = 7;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(123, 32);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(122, 67);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(58, 13);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblManucipality
+            // 
+            this.lblManucipality.AutoSize = true;
+            this.lblManucipality.Location = new System.Drawing.Point(114, 102);
+            this.lblManucipality.Name = "lblManucipality";
+            this.lblManucipality.Size = new System.Drawing.Size(66, 13);
+            this.lblManucipality.TabIndex = 1;
+            this.lblManucipality.Text = "Manucipality";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(156, 139);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 1;
+            this.lblCity.Text = "City";
+            // 
+            // lblBarangay
+            // 
+            this.lblBarangay.AutoSize = true;
+            this.lblBarangay.Location = new System.Drawing.Point(128, 175);
+            this.lblBarangay.Name = "lblBarangay";
+            this.lblBarangay.Size = new System.Drawing.Size(52, 13);
+            this.lblBarangay.TabIndex = 1;
+            this.lblBarangay.Text = "Barangay";
+            // 
+            // Street
+            // 
+            this.Street.AutoSize = true;
+            this.Street.Location = new System.Drawing.Point(145, 213);
+            this.Street.Name = "Street";
+            this.Street.Size = new System.Drawing.Size(35, 13);
+            this.Street.TabIndex = 1;
+            this.Street.Text = "Street";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Contact Number";
+            // 
+            // btnHomeownerSignUp
+            // 
+            this.btnHomeownerSignUp.Location = new System.Drawing.Point(217, 276);
+            this.btnHomeownerSignUp.Name = "btnHomeownerSignUp";
+            this.btnHomeownerSignUp.Size = new System.Drawing.Size(75, 23);
+            this.btnHomeownerSignUp.TabIndex = 8;
+            this.btnHomeownerSignUp.Text = "Done";
+            this.btnHomeownerSignUp.UseVisualStyleBackColor = true;
+            // 
+            // HomeownerSignUpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 311);
+            this.Controls.Add(this.btnHomeownerSignUp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Street);
+            this.Controls.Add(this.lblBarangay);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblManucipality);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.txtContactNumber);
+            this.Controls.Add(this.txtStreet);
+            this.Controls.Add(this.txtBarangay);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtMunicipality);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "HomeownerSignUpForm";
+            this.Text = "Homeowner Sign Up";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtMunicipality;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtBarangay;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.TextBox txtContactNumber;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblManucipality;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblBarangay;
+        private System.Windows.Forms.Label Street;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHomeownerSignUp;
+    }
+}
